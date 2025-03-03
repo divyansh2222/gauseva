138353f5df610528d665504e73720458fc3d7050
Divyansh Chouhan <118560135+divyansh2222@users.noreply.github.com>
2025-03-03 18:10:34 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav className="bg-[#F3D4B780] md:px-6 py-4 shadow-md">
       <div className="flex items-center justify-between mx-auto w-full px-4 md:px-0">
         {/* Logo */}
-        <img src={logo} alt="Logo" className="h-16" />
+        <Link to="/accomplish"><img src={logo} alt="Logo" className="h-16" /></Link>
 
         {/* Desktop Menu */}
         <div className="hidden sm:hidden md:flex md:items-center md:space-x-6 flex-grow justify-center gap-4">
@@ -39,7 +39,7 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden  flex flex-col items-center space-y-4 py-4  w-full">
           <Link className="text-lg font-semibold text-[#726253]" to="/">Home</Link>
-          <Link className="text-lg font-semibold text-[#726253]" to="/about">Known Us</Link>
+          <Link className="text-lg font-semibold text-[#726253]" to="/knowus">Known Us</Link>
           <Link className="text-lg font-semibold text-[#726253]" to="/contact">Contact Us</Link>
           <Link className="text-lg font-semibold text-[#726253]" to="/gallery">Gallery</Link>
           <LanguageSelector />
